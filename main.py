d34a6e1a4dc506190be5a21ff2d2120632ebc839
Tobi Ace <omoniyiadejengbe@gmail.com>
2024-09-17 18:59:00 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+
+
+def main():
+    pygame.init()
+    # print(
+        # f"""
+        # Starting asteroids!
+        # Screen width: {SCREEN_WIDTH}
+        # Screen height: {SCREEN_HEIGHT}
+        #   """)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+        
+
+if __name__ == "__main__":
+    main()
