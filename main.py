2f0804698cc1fb87d9474b4f3d3da986ca7657bf
Tobi Ace <omoniyiadejengbe@gmail.com>
2024-09-18 03:40:35 +0100

    FPS and Sprite setup

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
+    
     pygame.init()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     # print(
         # f"""
@@ -16,8 +21,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
         
 
 if __name__ == "__main__":
