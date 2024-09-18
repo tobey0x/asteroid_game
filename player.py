28be85aff3135e2d4a882d9e41d4ddad9735f576
Tobi Ace <omoniyiadejengbe@gmail.com>
2024-09-18 19:03:03 +0100

    Moving player around solved

--- player.py
+++ player.py
@@ -32,3 +32,13 @@ class Player(CircleShape):
 
       if keys[pygame.K_d]:
           self.rotate(dt)
+          
+      if keys[pygame.K_w]:
+        self.move(dt)
+
+      if keys[pygame.K_s]:
+        self.move(-dt)
+          
+  def move(self, dt):
+    self.position.x += PLAYER_SPEED * dt
+    self.position.y += PLAYER_SPEED * dt
