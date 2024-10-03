22638b74a3b0eb54939479cd51a7e6adf998c4cc
Tobi Ace <omoniyiadejengbe@gmail.com>
2024-10-03 18:28:57 +0100

    splitting the large asteroids on bullet impact

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from circleshape import *
 from constants import *
 
@@ -13,3 +14,18 @@ class Asteroid(CircleShape):
   def update(self, dt):
     self.position += self.velocity * dt
 
+  def split(self):
+    if self.radius <= ASTEROID_MIN_RADIUS:
+      return
+    random_angle = random.uniform(20, 50)
+    
+    vel1 = self.velocity.rotate(random_angle)
+    vel2 = self.velocity.rotate(-random_angle)
+    
+    new_radius = self.radius - ASTEROID_MIN_RADIUS
+    
+    roid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+    roid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+    roid_1.velocity = vel1 * 1.2
+    roid_2.velocity = vel2
+    self.kill()
--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if shot.collision(asteroid):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
                 
             
